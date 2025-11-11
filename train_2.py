eddcbb59c2d0e48b982e361f24d69583d122f39f
rikuto9111 <rikutokamegao@gmail.com>
2025-11-11 15:09:45 +0900

    Update train.py

--- train_2.py
+++ train_2.py
@@ -1,75 +1,79 @@
-def load_data(path):
+モデル1
 
-    global MIN_DATA_LENGTH
-    
-    max_len=0
-    min_len=sys.maxsize
-    
-    all_file=[]    
-    all_wavs=[]
-    all_labs=[]
-    
-    #ここから変更
-    scale_all = {} #人ごとのscale倍
-    rms_values_all = []#人ごとのrms
+model.add(BatchNormalization())#バッチ正規化
 
-    for target in subjects:#まず何より元のwavのrmsを計算してscale倍する
+model.add(Dropout(0.1))#ドロップアウト層
 
-        rms_values = []
+model.add(LSTM(LSTM_UNIT))#LSTM層
 
-        for fn in glob.glob(path+"*"+target+"*/*.wav"):#各々のutterance
+model.add(BatchNormalization())#バッチ正規化
+model.add(Dense(train_oneh.shape[1], activation='softmax'))#全結合層
 
-            wav, sr = librosa.load(fn, sr=None)
-            rms = np.sqrt(np.mean(wav**2))#アタランスごとのrms
-            rms_values.append(rms)
 
-        mean_rms = np.mean(rms_values) #それらの100文の和の平均
-        rms_values_all.append(mean_rms)#追加
-    
-    avg = sum(rms_values_all)/len(rms_values_all) #人ごとの平均rmsの平均
 
-    for i,target in enumerate(subjects):#被験者
-        scale_all[target] = (avg/rms_values_all[i])#それぞれの人の拡大すべきscaleを求める
-    
-    
-#ここまで　
+def extract(all_wavs):
 
-    for target in subjects:#subfileの中にそのtarget名の人の中のwavファイルが入ってて
-
-        sub_file=[]
-        sub_wavs=[]
-        sub_labs=[]
-
-        for fn in glob.glob(path+"*"+target+"*/*.wav"):#指定されたパターンにマッチする
-#ファイル名をすべてリストで返す　今pathはnewdata/haptic/
-            lab = load_label(fn)
-            
-            if lab != -1:#感情ラベル名に一致するファイルが一つもないとかつまりst_embaraとか以外
+    print("Feature extraction")
+    all_data=[]
+    for sub_wavs in all_wavs:
+        sub_data=[]
+        for wav in sub_wavs:
+#            print(".",end="")
+            if True:
+                ELEMENT_COUNT=N_MFCC
+                mfcc = librosa.feature.mfcc(y=wav, sr=SAMPLING_RATE, n_mfcc=N_MFCC, win_length=WINDOW_SIZE, hop_length=HOP_LENGTH)
+                data = mfcc.transpose()#転置
+            elif False:#実際には実行されない
+                ELEMENT_COUNT=int(WINDOW_SIZE/2)+1
+                stft = librosa.stft(wav, n_fft=WINDOW_SIZE, hop_length=HOP_LENGTH)
+                mag, phase = librosa.magphase(stft)
+#                mag_db = librosa.amplitude_to_db(mag)
+                data = mag[0:ELEMENT_COUNT,:].transpose()
+            else:
+                data = np.expand_dims(wav, axis=1)
                 
-                # 変更箇所1. 
-                wav = load_wave(fn)#wavファイル から波形をwavに読み込み格納する16khz
-                wav = wav*scale_all[target] #補正倍の正規化 ndarrayのbroadcast
+            sub_data.append(data)
+            
+        all_data.append(sub_data)
 
-                if wav.shape[0] != 0:#音声のサンプル数がwav.shape[0] sampling周波*時間
-                    if min_len > wav.shape[0]:
-                        min_len = wav.shape[0]
+    return all_data
+    
+モデル2
 
-                    if max_len < wav.shape[0]:#音声の時間をminからmaxで固定
-                        max_len = wav.shape[0]#wav[0]~wav[sample*時間] それぞれに振幅がはいっている
+model.add(BatchNormalization())
+    model.add(Dropout(0.1))
+    model.add(Conv1D(8, kernel_size=1, strides=1, padding='same', activation='relu'))
 
-                    sub_file.append(fn)
-                    sub_wavs.append(wav)
-                    sub_labs.append(lab)
-                    
-        all_file.append(sub_file)#all_fileには[("被験者名",subfile),[("被験者名",subfile)・・・]]がはいってるってことか
-        all_wavs.append(sub_wavs)
-        all_labs.append(sub_labs)
+    model.add(BatchNormalization())
+    model.add(Dropout(0.1))
+    model.add(Conv1D(16, kernel_size=3, strides=2, padding='same', activation='relu'))
 
-def load_wave(filepath):
-    print("loading: "+filepath)
-    wav, sr = librosa.load(filepath, sr=SAMPLING_RATE)
+    model.add(BatchNormalization())
+    model.add(Dropout(0.1))
+    model.add(Conv1D(16, kernel_size=3, strides=2, padding='same', activation='relu'))
+    model.add(Flatten())
+    model.add(BatchNormalization())
+    model.add(Dense(train_oneh.shape[1], activation='softmax'))
 
-    return wav
+モデル3
+    model.add(BatchNormalization())
+    model.add(Dropout(0.1))
+    model.add(Conv1D(8, kernel_size=1, strides=1, padding='same', activation='relu'))
 
-    MAX_DATA_LENGTH=max_len
-    MIN_DATA_LENGTH=min_len
+    model.add(BatchNormalization())
+    model.add(Dropout(0.1))
+    model.add(Conv1D(16, kernel_size=3, strides=2, padding='same', activation='relu'))
+
+    model.add(BatchNormalization())
+    model.add(Dropout(0.1))
+    model.add(Conv1D(16, kernel_size=3, strides=2, padding='same', activation='relu'))
+
+    model.add(BatchNormalization())
+    model.add(Dropout(0.1))
+    model.add(LSTM(LSTM_UNIT))
+
+    model.add(BatchNormalization())
+
+    model.add(Dense(train_oneh.shape[1], activation='softmax'))
+
+
