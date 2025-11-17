09cbeae243ba794f6ab57bd686fa84812f23fa79
rikuto9111 <rikutokamegao@gmail.com>
2025-11-17 18:35:44 +0900

    Update train_2.py

--- train_2.py
+++ train_2.py
@@ -1,3 +1,4 @@
+触覚
 モデル1
 
 model.add(BatchNormalization())#バッチ正規化
@@ -73,6 +74,79 @@ model.add(BatchNormalization())
     model.add(LSTM(LSTM_UNIT))
 
     model.add(BatchNormalization())
+
+
+触覚音声モデル 1(early confusion)
+
+merged = np.concatenate(([train_hap_data, train_aud_data]),axis = 2)
+merged_valid = np.concatenate(([valid_hap_data, valid_aud_data]),axis = 2)
+
+model = Sequential()
+model.add(LSTM(LSTM_UNIT))#LSTM層
+model.add(Dropout(0.1))#ドロップアウト層
+model.add(BatchNormalization())#バッチ正規化
+model.add(Dense(train_oneh.shape[1], activation='softmax'))
+
+ほとんど今までの構造
+
+これだと全然ダメ　触覚と音声の関係を学習していない
+
+触覚音声モデル 2(early confusion)
+
+merged = np.concatenate(([train_hap_data, train_aud_data]),axis = 2)
+merged_valid = np.concatenate(([valid_hap_data, valid_aud_data]),axis = 2)
+
+model = Sequential()
+
+model.add(Conv1D(16, kernel_size=3, padding='same'))#そのままmfccに対して並べたものを渡しても触覚と音声でmfccは異なるしそう言った関係性とか学びにくいかも？
+model.add(BatchNormalization())
+model.add(ReLU())
+model.add(Dropout(0.1))
+
+model.add(LSTM(32))#時系列はもちろんConvの後は残っている
+model.add(Dropout(0.1))
+
+model.add(Dense(train_oneh.shape[1], activation='softmax'))
+
+これは局所特長を先に抽出してからLSTMにかける感じ
+これだと精度が高い
+
+触覚音声モデル 3(early confusion)
+
+merged = np.concatenate(([train_hap_data, train_aud_data]),axis = 2)
+merged_valid = np.concatenate(([valid_hap_data, valid_aud_data]),axis = 2)
+
+model = Sequential()
+
+model.add(BatchNormalization())
+model.add(Dropout(0.1))
+model.add(Conv1D(8, kernel_size=1, strides=1, padding='same', activation='relu'))
+
+model.add(BatchNormalization())
+model.add(Dropout(0.1))
+model.add(Conv1D(16, kernel_size=3, strides=2, padding='same', activation='relu'))
+
+model.add(BatchNormalization())
+model.add(Dropout(0.1))
+model.add(Conv1D(16, kernel_size=3, strides=2, padding='same', activation='relu'))
+
+model.add(BatchNormalization())
+model.add(Dropout(0.1))
+model.add(LSTM(LSTM_UNIT))
+
+model.add(BatchNormalization())
+
+model.add(Dense(train_oneh.shape[1], activation='softmax'))
+
+中邑モデル3と同じ構造
+
+精度は悪くはないが、2のほうがよい
+
+
+
+
+
+
 
     model.add(Dense(train_oneh.shape[1], activation='softmax'))
 
